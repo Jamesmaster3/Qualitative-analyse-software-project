6aa66f9c635b2e5274b3034c9792eab08edcb6d9
Jamesmaster3 <120321282+Jamesmaster3@users.noreply.github.com>
2024-01-15 23:31:48 +0100

    Some testing and learning

--- Project InsightCode/MainWindow.xaml.cs
+++ Project InsightCode/MainWindow.xaml.cs
@@ -38,7 +38,27 @@ namespace Project_InsightCode
 
         private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e) // Have to find out what this is for later
         {
-            tagName.Text = "";
+            { };
         }
+
+        private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+  
+    }
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand("Exit", "Exit", typeof(CustomCommands), new InputGestureCollection()
+            {
+                new KeyGesture(Key.F4, ModifierKeys.Alt)
+            }
+            );
     }
 }
